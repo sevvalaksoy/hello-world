b78dcd22dd0dbba7b60bc9a6fdf1636f4b3eb195
sevvalaksoy <164859994+sevvalaksoy@users.noreply.github.com>
2024-05-14 13:36:09 +0300

    working space

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Hello World!")
+let arr = [12,54,7,64,56,264,56,423,1,36];
+function myfunction (arr, asc){
+    if(asc===true){
+        for (let i =0; i < arr.length; i++){
+            for (let j = 0; j < arr.length; j++){
+                if (arr[i]>arr[j]){
+                    const temp = arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+        }
+    } else {
+        for (let i =0; i < arr.length; i++){
+            for (let j = 0; j < arr.length; j++){
+                if (arr[i]<arr[j]){
+                    const temp = arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(myfunction(arr, true));
